39b81da009f1fbc65fc6cb13fe87643037660710
anuruddhhit07 <88274721+anuruddhhit07@users.noreply.github.com>
2024-05-01 17:24:16 +0900

    112324

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import {myFunction,OHLCVData } from "customcharts"
+import myFunction from "customcharts/src/index"
+console.log(myFunction);
+// console.log(OHLCVData);
 
 function App() {
   return (
